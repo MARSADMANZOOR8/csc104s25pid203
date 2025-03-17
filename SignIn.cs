def2f4baebeeb7c3d5637c7a3ee9d13741c83ab5
MARSAD MANZOOR <2022CS203@student.uet.edu.pk>
2025-03-17 23:11:14 +0500

    The sign In and Sign Up Page

---
+++ SignIn.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectB.LoginF
+{
+    public partial class SignIn: Form
+    {
+        public SignIn()
+        {
+            InitializeComponent();
+        }
+
+        private void SignIncs_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            new SignUp().Show();
+        }
+    }
+}
