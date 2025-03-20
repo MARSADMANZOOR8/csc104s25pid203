2a0457a36fc2fac2e905e21c1273cf2824e5fc3f
@Marsad134 <marsadmanzoor8@gmail.com>
2025-03-20 23:41:25 +0500

    The Class for the object which are later used in the list

---
+++ midProject/bl/FacultyBL.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace midProject.bl
+{
+    internal class FacultyBL
+    {
+        public string name { get; set; }
+        public string email { get; set; }
+        public string password;
+        public string contact { get; set; }
+        public string designation { get; set; }
+        public string research_area { get; set; }
+        public int teaching_hours { get; set; }
+
+        public FacultyBL(string name, string email, string contact, string designation, string research_area, int teaching_hours)
+        {
+            this.name = name;
+            this.email = email;
+            this.contact = contact;
+            this.designation = designation;
+            this.research_area = research_area;
+            this.teaching_hours = teaching_hours;
+        }
+
+        public FacultyBL(string name, string email, string password, string contact, string designation, string research_area, int teaching_hours)
+        {
+            this.name = name;
+            this.email = email;
+            this.contact = contact;
+            this.password = password;
+            this.designation = designation;
+            this.research_area = research_area;
+            this.teaching_hours = teaching_hours;
+        }
+    }
+}
---
+++ midProject/bl/UsersBL.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace midProject.bl
+{
+    internal class UsersBL
+    {
+        public string username;
+        public string password;
+        public string email;
+        public string role;
+
+        public UsersBL(string username, string password, string email, string role)
+        {
+            this.username = username;
+            this.password = password;
+            this.email = email;
+            this.role = role;
+        }
+
+        public UsersBL(string username, string email, string role)
+        {
+            this.username = username;
+            this.email = email;
+            this.role = role;
+        }
+    }
+}
