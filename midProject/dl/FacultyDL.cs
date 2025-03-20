cbb5ac73ae1c082e31456a5e673a5e05934f68e6
@Marsad134 <marsadmanzoor8@gmail.com>
2025-03-20 16:28:39 +0500

    "Implement the dl folder to keep all learning functions in separate role-based files, ensuring that each file maintains a distinct working principle.

---
+++ midProject/dl/FacultyDL.cs
@@ -0,0 +1,135 @@
+﻿using midProject.bl;
+using midProject.UI.windowForm;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace midProject.dl
+{
+    internal class FacultyDL
+    {
+        public static List<FacultyBL> faculties = new List<FacultyBL>();
+
+        public static void AddFaculty(FacultyBL faculty)
+        {
+            faculties.Add(faculty);
+        }
+
+        //public static void LoadFaculty()
+        //{
+        //    var connection = DataBaseHelper.GetConnection();
+        //    connection.Open();
+        //    string query = "SELECT username, email, lookup.value as RoleValue\r\nFROM users \r\nLEFT JOIN lookup ON lookup.lookup_id = users.role_id;";
+        //    SqlCommand command = new SqlCommand(query, connection);
+        //    SqlDataReader reader = command.ExecuteReader();
+
+        //    while (reader.Read())
+        //    {
+        //        string username = reader["username"].ToString();
+        //        string email = reader["email"].ToString();
+        //        string role = reader["RoleValuee"].ToString();
+        //        UsersBL user = new UsersBL(username, email, role);
+        //        AddFaculty(user);
+        //    }
+
+        //    reader.Close();
+        //}
+
+        public static int GetInsertedID(FacultyBL faculty)
+        {
+            int insertedUserId = -1;
+            using (SqlConnection connection = DataBaseHelper.GetConnection())
+            {
+                connection.Open();
+                int roleId = 2;
+                string insertQuery = @"
+            INSERT INTO [users] (username, email, password_hash, role_id) 
+            OUTPUT INSERTED.user_id 
+            VALUES (@name, @email, @password, @roleId)";
+
+                string hash_password = UsersDL.HashPassword(faculty.password);
+                using (SqlCommand cmd = new SqlCommand(insertQuery, connection))
+                {
+                    cmd.Parameters.AddWithValue("@name", faculty.name);
+                    cmd.Parameters.AddWithValue("@email", faculty.email);
+                    cmd.Parameters.AddWithValue("@password", hash_password);
+                    cmd.Parameters.AddWithValue("@roleId", roleId);
+
+                    object result = cmd.ExecuteScalar();
+
+                    if (result != null)
+                    {
+                        insertedUserId = Convert.ToInt32(result);
+                    }
+                }
+            }
+
+            return insertedUserId;
+        }
+
+
+        public static int GetDesignationID(string designation)
+        {
+            int designation_id = -1;
+            var connection = DataBaseHelper.GetConnection();
+            connection.Open();
+            string query = "Select lookup_id\r\nfrom lookup\r\nwhere value = @value";
+            using (SqlCommand cmd = new SqlCommand(query, connection))
+            {
+                cmd.Parameters.AddWithValue("@value", designation);
+                object result = cmd.ExecuteScalar();
+
+                if (result != null)
+                {
+                    designation_id = Convert.ToInt32(result);
+                }
+
+            }
+            return designation_id;
+        }
+
+        public static int InsertIntoFaculty(FacultyBL faculty)
+        {
+            var connection = DataBaseHelper.GetConnection();
+            connection.Open();
+            string insertQuery = @"
+            INSERT INTO [faculty] (name, email, contact, designation_id, research_area, total_teaching_hours, user_id) 
+            VALUES (@name, @email, @contact, @designation_id, @research_area, @total_teaching_hours, @user_id)";
+            int user_id = GetInsertedID(faculty);
+            int designation_id = GetDesignationID(faculty.designation);
+            if(user_id > 0 && designation_id>0)
+            {
+                using (SqlCommand cmd = new SqlCommand(insertQuery, connection))
+                {
+                    cmd.Parameters.AddWithValue("@name", faculty.name);
+                    cmd.Parameters.AddWithValue("@email", faculty.email);
+                    cmd.Parameters.AddWithValue("@contact", faculty.contact);
+                    cmd.Parameters.AddWithValue("@designation_id", designation_id);
+                    cmd.Parameters.AddWithValue("@research_area", faculty.research_area);
+                    cmd.Parameters.AddWithValue("@total_teaching_hours", faculty.teaching_hours);
+                    cmd.Parameters.AddWithValue("@user_id", user_id);
+
+                    int rowsAffected = cmd.ExecuteNonQuery();
+                    if (rowsAffected > 0)
+                    {
+                        return 0;
+
+                    }
+                    else
+                    {
+                        return 1;
+                    }
+                }
+            }
+            else
+            {
+                return 2;
+            }
+        }
+
+
+    }
+}
---
+++ midProject/dl/UsersDL.cs
@@ -0,0 +1,108 @@
+﻿using midProject.bl;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+using System.Xml.Linq;
+
+namespace midProject.dl
+{
+    internal class UsersDL
+    {
+        public static List<UsersBL> users = new List<UsersBL>();
+
+        public static void AddUser(UsersBL user)
+        {
+            users.Add(user);
+        }
+
+        public static void LoadUsers()
+        {
+            var connection = DataBaseHelper.GetConnection();
+            connection.Open();
+            string query = "SELECT username, email, lookup.value as RoleValue\r\nFROM users \r\nLEFT JOIN lookup ON lookup.lookup_id = users.role_id;";
+            SqlCommand command = new SqlCommand(query, connection);
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                string username = reader["username"].ToString();
+                string email = reader["email"].ToString();
+                string role = reader["RoleValuee"].ToString();
+                UsersBL user = new UsersBL(username, email, role);
+                AddUser(user);
+            }
+
+            reader.Close();
+        }
+
+
+        public static string HashPassword(string password)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                StringBuilder sb = new StringBuilder();
+                foreach (byte b in bytes)
+                {
+                    sb.Append(b.ToString("x2")); // Convert byte to hex string
+                }
+                return sb.ToString();
+            }
+        }
+
+
+        public static int InsertUserData(UsersBL user)
+        {
+            var connection = DataBaseHelper.GetConnection();
+            connection.Open();
+            string query = "SELECT lookup_id FROM lookup WHERE category = 'User Roles' AND value = @role";
+            int roleId = -1;
+            using (SqlCommand cmd = new SqlCommand(query, connection))
+            {
+                cmd.Parameters.AddWithValue("@role", user.role);
+                object result = cmd.ExecuteScalar();
+
+                if (result != null)
+                {
+                    roleId = Convert.ToInt32(result);
+                }
+                else
+                {
+                    return 1;
+                }
+
+            }
+
+            string hashedPassword = HashPassword(user.password);
+
+            string insertQuery = "INSERT INTO [users] (username, email, password_hash, role_id) VALUES (@name, @email, @password, @roleId)";
+
+            using (SqlCommand cmd = new SqlCommand(insertQuery, connection))
+            {
+                cmd.Parameters.AddWithValue("@name", user.username);
+                cmd.Parameters.AddWithValue("@email", user.email);
+                cmd.Parameters.AddWithValue("@password", hashedPassword);
+                cmd.Parameters.AddWithValue("@roleId", roleId);
+
+                int rowsAffected = cmd.ExecuteNonQuery();
+
+                if (rowsAffected > 0)
+                {
+                    return 0;
+
+                }
+                else
+                {
+                    return 2;
+                }
+            }
+        }
+    }
+}
